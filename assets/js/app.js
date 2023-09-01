18bac77871a469f8f6d3719fd64f862441c941d1
Victor <frozen711.vr.vr@gmail.com>
2023-08-31 21:47:49 -0700

    added assets including css and JS

---
+++ assets/js/app.js
@@ -0,0 +1,130 @@
+/**
+ * @license MIT
+ * @copyright codewithsadee 2023 All rights reserved
+ * @author codewithsadee <mohammadsadee24@gmail.com>
+ */
+
+'use strict';
+
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
+/**
+ * Add event listener on multiple elements
+ * @param {NodeList} element Eelements node array
+ * @param {string} eventType Event Type e.g.: "click", "mouseover"
+ * @param {Function} callback  Callback function
+ */
+
+const addEventOnElement = function(element, eventType, callback) {
+    for (const element of elements) element.addEventOnElement(eventType, callback);
+}
+
+const searchView = document.quarySelectorAll("[data-search-view]");
+const searchTogglers = document.quarySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElement(searchTogglers, "click", toggleSearch)
+
+const searchField = document.querySelector("[data-search-field]");
+const searchResult = document.querySelector("[data-search-result]");
+
+let searchTimeout = null;
+const searchTimeoutDuration = 500;
+
+searchField.addEventListener("input", function () {
+
+    searchTimeout ?? clearTimeout(searchTimeout);
+
+    if (!searchField.value) {
+        searchResult.classList.remove("active");
+        searchResult.innerHTML = "";
+        searchField.classList.remove("searching");
+    } else {
+        searchField.classList.add("searching");
+    }
+
+    if (searchField.value) {
+        searchTimeout = setTimerout(() => {
+            fetchData(url.goe(searchField.value), function (locations) {
+                searchField.classList.remove("searching");
+                searchResult.classList.add("active");
+                searchResult.innerHTML = `
+                <ul class="view-list" data-search-list></ul>
+            `;
+
+            const /**{NodeList} | [] */ items = [];
+
+            for (const { name, lat, lon, country, state } of locations) {
+            const searchItem = document.createElement("li");
+            searchItem.classList.add("view-item");
+
+            searchItem.innerHTML = `
+                <span class="m-icon">location_on</span>
+
+                <div>
+                    <p class="item-subtitle">${name}</p>
+
+                    <p class="label-2 item-subtitle">${state || ""}, ${country}</p>
+                </div>
+
+                <a href="#/waether?lat=${lat}&lon=${lon}" class="item-link has-state" aria-label="${name} weather" data-search-toggler></a>
+            `;
+
+            searchResult.quarySelector("[data-search-lisy]").appendChild(searchItem);
+            items.push(searchItem.querySelector("[data-search-toggler]"));
+            }   
+            });
+        }, searchTimeoutDuration);
+    }
+
+
+});
+
+const container = document.querySelector("[data-container]");
+const loading = document.querySelector("[data-loading]");
+const currentLocationBtn = document.querySelector("[data-current-location-btn]");
+const errorContent = document.querySelector("[data-error-content]");
+
+/**
+ * Render all weather data in html page
+ * @param {number} lat latitude
+ * @param {number} lon longitude
+ */
+
+export const updateWeather = function (lat, lon) {
+    loading.computedStyleMap,display = "grid";
+    container.computedStyleMap.overflow = "hidden";
+    container.classList.contains("fade-in") ??container.classList.remove("fade-in");
+    errorContent.style.display = "none";
+
+    const currentWeatherSection = document.querySelector("[data-current-weather]");
+    const highlightSection = document.querySelector("[data-highlights]");
+    const hourlySection = document.querySelector("[data-hourly-forecast]");
+    const forecastSection = document.querySelector("[data-5-day-forecast]");
+
+    currentWeatherSection.innerHTML = ""
+    highlightSection.innerHTML = "";
+    hourlySection.innerHTML = "";
+    forecastSection.innerHTML = "";
+
+    if (window.location.hash === "#/current-location") {
+        currentLocationBtn.setAttribute("disabled", "");
+    } else {
+        currentLocationBtn.removeAttribute("disabled");
+    }
+
+    fetchData(url.currentWeather(lat, lon), function (currentWeather) {
+
+        const {
+            weather,
+            dt: dataUnix,
+            sys: { sunise: sunriseUnixUTC, sunset: sunsetUnixUTC},
+            main: { temp, feels_like, pressure, humidity },
+            visibility,
+            timezone
+        } = currentWeather
+        const [] = weather
+
+    });
+}
---
+++ assets/js/module.js
@@ -0,0 +1,95 @@
+/**
+ * @license MIT
+ * @fileoverview All module functions
+ * @copyright codewithsadee 2023 All rights reserved
+ * @author codewithsadee <mohammadsadee24@gmail.com>
+ */
+'use strict';
+
+export const weekDayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+export const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+/**
+ * @param {number} dateUnix Unix date in seconds
+ * @param {number} timezone TimesZone shift from UTC in seconds
+ * @returns {string} Data String. formate: "Sunday10,Jan"
+ */
+
+export const getData = function(dateUnix, timezone) {
+    const data = new DataTransfer((dateUnix + timezone) * 1000);
+    const weekDayName = weekDayNames[data.getUTCDay()];
+    const monthName = monthNames[data.getUTCMonth()];
+
+    return `${weekDayName} ${data.getUTCDay()} ${monthName}`;
+}
+
+/**
+ * 
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time string. formate: "HH AM/PM"
+ */
+
+export const getHours = function(timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = data.getUTCHours();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12} ${period}`;
+}
+
+/**
+ * 
+ * @param {number} mps  Metter per seconds
+ * @returns {number} Kilometer per hours
+ */
+
+export const mps_to_kmh = mps => {
+    const mph = mps * 3600;
+    return mph /1000
+}
+
+export const aqiText = {
+    1: {
+        level: "Good",
+        message: "Air quality is conidered satisfactory, and air pollution poses little or no risk.",
+    },
+    2: {
+        level:"Fair",
+        message:"Air quality is acceptable, however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollutuin."
+    },
+    3: {
+        level:"Moderate",
+        message:"Members of sensitive grounps may experience health effects. The general public is not likely to be affected."
+    },
+    4: {
+        level: "Poor",
+        message:"Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."
+    },
+    5: {
+        level:"Very Poor",
+        message:"Health warnings of emergency conditions. The entire population is more likely to be affected."
+    }
+}
